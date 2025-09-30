767e79fc84ad1caa3b77d263869f1c22fbb35f70
MuhammadRamzy <mhdramzy777@gmail.com>
2025-10-01 02:01:32 +0530

    License Update

--- backend.py
+++ backend.py
@@ -15,9 +15,23 @@
                        ::::::::::::....::::.....:::::::::::                                    
                         .:::::::::::::::::::::::::::::::::.        
 
-                 © Copyright of Ignition Software Department
-"""
+                    Copyright (c) 2025 Ignition Software Department
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3, with the additional restriction
+that this software may not be used for commercial purposes without
+explicit written permission from the authors.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+"""
 ####################################################################################################
 # File:        backend.py
 # Author:      Shawn Liju Thomas
